698787ab191c8df96149c4678bae56f25b35426e
mvmaniac <mvmaniaz@gmail.com>
2019-10-30 00:43:11 +0900

    [변경] Spring boot 버전 변경으로 인한 테스트코드 수정

--- service-internal-common/src/test/java/io/devfactory/ServiceInternalCommonApplicationTest.java
+++ service-internal-common/src/test/java/io/devfactory/ServiceInternalCommonApplicationTest.java
@@ -1,18 +1,13 @@
 package io.devfactory;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.*;
-
-@RunWith(SpringRunner.class)
 @SpringBootTest
-public class ServiceInternalCommonApplicationTest {
+class ServiceInternalCommonApplicationTest {
 
     @Test
-    public void contextLoads() {
+    void contextLoads() {
     }
 
 }
--- service-internal-common/src/test/java/io/devfactory/member/MemberRepositoryTest.java
+++ service-internal-common/src/test/java/io/devfactory/member/MemberRepositoryTest.java
@@ -1,24 +1,21 @@
 package io.devfactory.member;
 
 import lombok.extern.slf4j.Slf4j;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Slf4j
-@RunWith(SpringRunner.class)
 @DataJpaTest
-public class MemberRepositoryTest {
+class MemberRepositoryTest {
 
     @Autowired
     private MemberRepository memberRepository;
 
     @Test
-    public void 회원_가입을_할_수_있다() {
+    void 회원_가입을_할_수_있다() {
         // given
         String name = "dev";
         String email = "dev@gmail.com";
