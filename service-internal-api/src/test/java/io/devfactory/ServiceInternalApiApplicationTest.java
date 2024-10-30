945c835e24147e5143ad68f3faf02326517598b8
yhs <mvmaniaz@gmail.com>
2024-10-30 23:35:30 +0900

    Update suppression warnings in test classes

--- service-internal-api/src/test/java/io/devfactory/ServiceInternalApiApplicationTest.java
+++ service-internal-api/src/test/java/io/devfactory/ServiceInternalApiApplicationTest.java
@@ -3,7 +3,7 @@ package io.devfactory;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SuppressWarnings("squid:S2699")
+@SuppressWarnings("squid:S1186")
 @SpringBootTest
 class ServiceInternalApiApplicationTest {
 
